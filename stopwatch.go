1bc09a9eaec496a9286a77d49b2528b263e7317e
Izhari Ishak Aksa <izharishaksa@gmail.com>
2021-08-23 19:07:00 +0700

    Create stopwatch.go

---
+++ stopwatch.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	var pressed = make([]int32, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanln(&pressed[i])
+	}
+	if n%2 == 1 {
+		fmt.Println("still running")
+	} else {
+		var result int32
+		for i := len(pressed) - 1; i >= 0; i -= 2 {
+			result += pressed[i] - pressed[i-1]
+		}
+		fmt.Println(result)
+	}
+}
