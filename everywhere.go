53d59a8b221a9b817b02533bb0f5480111937734
Izhari Ishak Aksa <izharishaksa@gmail.com>
2021-08-24 10:12:07 +0700

    Create everywhere.go

---
+++ everywhere.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var T, n int
+	var city string
+	fmt.Scanln(&T)
+
+	for t := 0; t < T; t++ {
+		var visited = map[string]bool{}
+		fmt.Scanln(&n)
+		for i := 0; i < n; i++ {
+			fmt.Scanln(&city)
+			visited[city] = true
+		}
+		fmt.Println(len(visited))
+	}
+}
