dfb1688c72e3459f39eb3343b3fdd547a1867184
Izhari Ishak Aksa <izharishaksa@gmail.com>
2021-08-25 07:38:50 +0700

    Create missingnumbers.go

---
+++ missingnumbers.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, last int
+	var isCounted [201]bool
+	fmt.Scanln(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scanln(&last)
+		isCounted[last] = true
+	}
+	var count = 0
+	for i := 1; i < last; i++ {
+		if !isCounted[i] {
+			fmt.Println(i)
+			count++
+		}
+	}
+	if count == 0 {
+		fmt.Println("good job")
+	}
+
+}
