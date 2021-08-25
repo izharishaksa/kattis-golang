58cd53f931809c42e05d0938875433ddbebaf79a
Izhari Ishak Aksa <izharishaksa@gmail.com>
2021-08-25 11:08:29 +0700

    Create cold.go

---
+++ cold.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, total, degree int
+	fmt.Scanln(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &degree)
+		if degree < 0 {
+			total++
+		}
+	}
+	fmt.Println(total)
+
+}
