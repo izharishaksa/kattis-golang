642e150ff0ffd5595f34363e0afbfe50e0e1dc79
Izhari Ishak Aksa <izharishaksa@gmail.com>
2021-08-25 10:53:16 +0700

    Create pet.go

---
+++ pet.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var winner, max int
+	for i := 0; i < 5; i++ {
+		var curScore, score int
+		for j := 0; j < 4; j++ {
+			fmt.Scanf("%d", &score)
+			curScore += score
+		}
+		if curScore > max {
+			max = curScore
+			winner = i + 1
+		}
+	}
+	fmt.Println(winner, max)
+}
